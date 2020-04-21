d35f3b9fd9526a62572336e92833fd8e34dd15d5
rijen <rijen@redhat.com>
2020-04-21 10:41:02 -0400

    made a feature change

--- cogwheel.js
+++ cogwheel.js
@@ -1,4 +1,4 @@
 console.log('this is a cogwheel')
 console.log('new feature')
-console.log('third feature added')
+console.log('third feature added and feature requested')
 console.log('this cogwheel is now red')
