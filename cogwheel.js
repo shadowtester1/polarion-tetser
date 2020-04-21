3f0fc90eb56b49da1f8ae9d85054e2f464ca953f
rijen <rijen@redhat.com>
2020-04-21 10:42:04 -0400

    TEST-79 made a feature change

--- cogwheel.js
+++ cogwheel.js
@@ -1,4 +1,4 @@
 console.log('this is a cogwheel')
 console.log('new feature')
-console.log('third feature added and feature requested')
+console.log('Test 79 third feature added and feature requested')
 console.log('this cogwheel is now red')
